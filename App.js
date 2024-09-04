646dfec9eaf2bd89e7e4635b177f6f5c7bad69b7
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2024-09-05 01:19:08 +0400

    Read all sheets

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 const getProductDetails = require('./ProductDetailsFetcher');
 const XLSX = require('xlsx');
 
-const getProductData = () => {
-    const productData = [];
-
-    const workbook = XLSX.readFile('./Test.xlsx');
-    const sheetName = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[sheetName];
+const getProductData = (worksheet) => {
     const rows = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
+    const productData = [];
     for (let i = 1; i < rows.length; i++) {
         productData.push({
             'code': rows[i][0],
@@ -21,22 +17,31 @@ const getProductData = () => {
 }
 
 const main = async () => {
-    const productsData = getProductData();
+    const workbook = XLSX.readFile('./Test.xlsx');
 
-    for (let i = 0; i < productsData.length; i++) {
-        const singleProduct = productsData[i];
-        try {
-            const productDetails = await getProductDetails(singleProduct.url);
-            
-            const isProductInStock = productDetails.some(sku => sku.stock > 0);
-            if (!isProductInStock) {
-                console.log(`Product with code: ${singleProduct.code} is out of stock`, productDetails);
-            } else {
-                const excelRow = i + 1;
-                console.log(`Product with code: ${singleProduct.code} is in stock. Row: ${excelRow}`, productDetails);
+    const sheetNames = workbook.SheetNames;
+    for (let i = 0; i < sheetNames.length; i++) {
+        const currentSheetName = sheetNames[i];
+        const currentSheet = workbook.Sheets[currentSheetName];
+
+        console.log(`Processing sheet: ${currentSheetName}`);
+        
+        const productsData = getProductData(currentSheet);
+        for (let i = 0; i < productsData.length; i++) {
+            const singleProduct = productsData[i];
+            try {
+                const productDetails = await getProductDetails(singleProduct.url);
+                
+                const isProductInStock = productDetails.some(sku => sku.stock > 0);
+                if (!isProductInStock) {
+                    console.log(`Product with code: ${singleProduct.code} is out of stock`);
+                } else {
+                    const excelRow = i + 2;
+                    console.log(`Product with code: ${singleProduct.code} is in stock. Row: ${excelRow}`);
+                }
+            } catch (error) {
+                console.log("Could not fetch the details for product with code: " + singleProduct.code);
             }
-        } catch (error) {
-            console.log("Could not fetch the details for product with code: " + singleProduct.code);
         }
     }
 }
